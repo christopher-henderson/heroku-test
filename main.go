b8d8098a82491b3803f5415a498889de3f6cbdef
christopher-henderson <chris@chenderson.org>
2017-12-11 15:09:10 -0700

    lol

--- main.go
+++ main.go
@@ -5,24 +5,15 @@
 package main
 
 import (
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
-	"crypto/x509"
-	"crypto/x509/pkix"
 	"encoding/json"
-	"encoding/pem"
 	"flag"
 	"fmt"
 	"io"
 	"log"
-	"math/big"
-	"net"
 	"net/http"
 	"os"
 	"path"
 	"sync"
-	"time"
 
 	"github.com/mozilla/OneCRL-Tools/ccadb"
 	"github.com/mozilla/OneCRL-Tools/certdata"
@@ -56,12 +47,6 @@ var unmatchedTrustPath string
 var unmatchedUntrustedPath string
 
 var serverMode bool
-var addr string
-var port string
-var encrypt bool
-var public string
-var private string
-var cert string
 
 func init() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
@@ -71,17 +56,11 @@ func init() {
 	flag.StringVar(&ccadbURL, "ccadburl", ccadb.URL, "URL to CCADB report file.")
 	flag.StringVar(&outDir, "o", "", "Path to the output directory.")
 	flag.BoolVar(&serverMode, "serve", false, "Start in server mode.")
-	flag.StringVar(&addr, "address", "127.0.0.1", "Address to listen on while in server mode.")
-	flag.StringVar(&port, "port", "80", "Port to listen on while in server mode.")
 	flag.Parse()
 
 	matchedPath = path.Join(outDir, matched)
 	unmatchedTrustPath = path.Join(outDir, unmatchedTrusted)
 	unmatchedUntrustedPath = path.Join(outDir, unmatchedUntrusted)
-
-	public = path.Join(CertDir, Public)
-	private = path.Join(CertDir, Private)
-	cert = path.Join(CertDir, Cert)
 }
 
 // Functions used for single run mode.
@@ -173,92 +152,6 @@ func parse(src func() io.ReadCloser, parser func(io.Reader) ([]*certdataDiffCCAD
 
 // Functions used for server mode.
 
-// CertTemplate generates a bare minimum template use for x509.CreateCertificate.
-// Credit to https://ericchiang.github.io/post/go-tls/ for saving a lot of time figuring out
-// the bare minimum required.
-func CertTemplate() (*x509.Certificate, error) {
-	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
-	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
-	if err != nil {
-		return nil, err
-	}
-
-	tmpl := x509.Certificate{
-		SerialNumber:          serialNumber,
-		Subject:               pkix.Name{Organization: []string{"Mozilla"}},
-		SignatureAlgorithm:    x509.ECDSAWithSHA384,
-		NotBefore:             time.Now(),
-		NotAfter:              time.Now().Add(time.Hour * 24 * 31 * 12 * 100), // Why not 100 years?
-		BasicConstraintsValid: true,
-	}
-	tmpl.IsCA = false
-	tmpl.KeyUsage = x509.KeyUsageCertSign | x509.KeyUsageDigitalSignature
-	tmpl.ExtKeyUsage = []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth}
-	tmpl.IPAddresses = []net.IP{net.ParseIP("127.0.0.1")}
-	return &tmpl, nil
-}
-
-// GenerateTLSCert generates an ECDSA key pair over a P384 curve, as well as a
-// self signed certificate, and writes the encoded PEMs to the provided file paths.
-//
-// Many - MANY - erros may occur. This function logs and exits on any errors.
-func GenerateTLSCertOrDie(publicName, privateName, certName string) {
-	// Generate an ECDSA key pair.
-	private, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
-	if err != nil {
-		log.Fatal(err)
-	}
-	public := private.Public()
-	// CreateCertificate requires that a "template" be given, so generate an empty cert object
-	// with the basic required fields.
-	template, err := CertTemplate()
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Self Sign.
-	cert, err := x509.CreateCertificate(rand.Reader, template, template, public, private)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Marshal in memory PEMs to bytes.
-	privKey, err := x509.MarshalECPrivateKey(private)
-	if err != nil {
-		log.Fatal(err)
-	}
-	pubKey, err := x509.MarshalPKIXPublicKey(public)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Encode and write the key pair and self signed cert to disk.
-	privF, err := os.Create(privateName)
-	defer privF.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-	pubF, err := os.Create(publicName)
-	defer pubF.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-	certF, err := os.Create(certName)
-	defer certF.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = pem.Encode(privF, &pem.Block{"EC PRIVATE KEY", make(map[string]string, 0), privKey})
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = pem.Encode(pubF, &pem.Block{"EC PUBLIC KEY", make(map[string]string, 0), pubKey})
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = pem.Encode(certF, &pem.Block{"CERTIFICATE", make(map[string]string, 0), cert})
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
 // SimpleEntry is a subset of the certdataDiffCCADB.Entry use to provide a simpler
 // view of the certdata file while in server mode.
 type SimpleEntry struct {
@@ -309,8 +202,19 @@ func ListCertdata(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
-// Runner functions for either single run mode or server mode.
+// Runner function for starting the server.
+func serve() {
+	log.Println("Starting in server mode.")
+	// Add handlers.
+	http.HandleFunc("/certdata", ListCertdata)
+	// Start up the server.
+	p := os.Getenv("PORT")
+	address := fmt.Sprintf("%v:%v", "", p)
+	log.Printf("Listening on %v\n", address)
+	log.Fatal(http.ListenAndServe(address, nil))
+}
 
+// Runner functions for either single run mode or server mode.
 func singleRun() {
 	cdResult := make(chan []*certdataDiffCCADB.Entry)
 	CCADBResult := make(chan []*certdataDiffCCADB.Entry)
@@ -328,21 +232,6 @@ func singleRun() {
 	go writeJSON(unmatchedT, unmatchedTrustPath, wg)
 	go writeJSON(unmatchedUT, unmatchedUntrustedPath, wg)
 	wg.Wait()
-}
-
-func serve() {
-	log.Println("Starting in server mode.")
-	// Add handlers.
-	http.HandleFunc("/hello", ListCertdata)
-	// Encrypt, but we don't particularly need authetication,
-	// so self signed cert it is.
-	log.Printf("Generating new TLS certificate.")
-	GenerateTLSCertOrDie(public, private, cert)
-	// Start up the server.
-	p := os.Getenv("PORT")
-	address := fmt.Sprintf("%v:%v", "", p)
-	log.Printf("Listening on %v\n", address)
-	log.Fatal(http.ListenAndServe(address, nil))
 }
 
 func main() {
